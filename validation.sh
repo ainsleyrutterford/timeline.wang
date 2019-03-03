f8b2a96adcc302eb5b698c6a3e24cf763f81dcf9
Ainsley Rutterford <ar16478@my.bristol.ac.uk>
2019-03-03 00:40:33 +0000

    Updated xml:lang and lang in public/index.html

--- validation.sh
+++ validation.sh
@@ -2,3 +2,7 @@
 
 echo "Running validation check online..."
 java -Dnu.validator.client.content-type=application/xhtml+xml -Dnu.validator.client.port=80 -Dnu.validator.client.host=html5.validator.nu -cp node_modules/vnu-jar/build/dist/vnu.jar nu.validator.client.HttpClient public/index.html
+
+# Once we go public, we may need to download files from our server using wget,
+# running the command above, and then deleting the files locally afterwards?
+# Or something like that.
