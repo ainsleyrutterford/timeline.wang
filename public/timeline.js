e468565cd8432fc2ae407fd214047925638d833f
Ainsley Rutterford <ar16478@my.bristol.ac.uk>
2019-04-21 00:10:56 +0100

    Added timeline.js with simple animation

---
+++ public/timeline.js
@@ -0,0 +1,40 @@
+"use strict";
+
+var canvas = document.getElementById("canvas");
+var ctx = canvas.getContext('2d');
+
+var ball = {
+  x: 100,
+  y: 100,
+  vx: 5,
+  vy: 2,
+  radius: 25,
+  color: 'blue',
+  draw: function() {
+    ctx.beginPath();
+    ctx.arc(this.x, this.y, this.radius, 0, Math.PI * 2, true);
+    ctx.closePath();
+    ctx.fillStyle = this.color;
+    ctx.fill();
+  }
+};
+
+function draw() {
+  canvas.width = window.innerWidth;
+  canvas.height = window.innerHeight;
+  ctx.clearRect(0,0, canvas.width, canvas.height);
+  ball.draw();
+  ball.x += ball.vx;
+  ball.y += ball.vy;
+
+  if (ball.y + ball.vy > canvas.height || ball.y + ball.vy < 0) {
+    ball.vy = -ball.vy;
+  }
+  if (ball.x + ball.vx > canvas.width || ball.x + ball.vx < 0) {
+    ball.vx = -ball.vx;
+  }
+
+  window.requestAnimationFrame(draw);
+}
+
+window.requestAnimationFrame(draw);
