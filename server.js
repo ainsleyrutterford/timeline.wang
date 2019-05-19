9560504e8c944952732fadb9ef3bdc37a9e10daa
Ainsley Rutterford <ar16478@my.bristol.ac.uk>
2019-05-19 23:42:59 +0100

    server.js now contains express server.

--- server.js
+++ server.js
@@ -1,180 +1,129 @@
 "use strict";
 
-// Run a node.js web server for local development of a static web site. Create a
-// site folder, put server.js in it, create a sub-folder called "public", with
-// at least a file "index.html" in it. Start the server with "node server.js &",
-// and visit the site at the address printed on the console.
-//     The server is designed so that a site will still work if you move it to a
-// different platform, by treating the file system as case-sensitive even when
-// it isn't (as on Windows and some Macs). URLs are then case-sensitive.
-//     All HTML files are assumed to have a .html extension and are delivered as
-// application/xhtml+xml for instant feedback on XHTML errors. Content
-// negotiation is not implemented, so old browsers are not supported. Https is
-// not supported. Add to the list of file types in defineTypes, as necessary.
+// Import modules.
+var express     = require('express');
+var sqlite      = require("sqlite");
+var helmet      = require('helmet');
+var compression = require('compression');
+var passport    = require('passport');
+var Strategy    = require('passport-local').Strategy;
 
-// Change the port to the default 80, if there are no permission issues and port
-// 80 isn't already in use. The root folder corresponds to the "/" url.
-let port = 8080;
-let root = "./public"
+// Create an express application.
+var app = express();
 
-// Load the library modules, and define the global constants and variables.
-// Load the promises version of fs, so that async/await can be used.
-// See http://en.wikipedia.org/wiki/List_of_HTTP_status_codes.
-// The file types supported are set up in the defineTypes function.
-// The paths variable is a cache of url paths in the site, to check case.
-let http = require("http");
-let fs = require("fs").promises;
-let OK = 200, NotFound = 404, BadType = 415, Error = 500;
-let types, paths;
+// Use these middleware modules.
+app.use(helmet());
+app.use(compression());
 
-// Reuire sqlite
-var sqlite = require("sqlite");
+passport.use(new Strategy(
+  function(username, password, cb) {
+    find_by_username(username, function(err, user) {
+      if (err) { return cb(err); }
+      if (!user) { return cb(null, false); }
+      if (user.password != password) { return cb(null, false); }
+      return cb(null, user);
+    });
+  }));
 
-// Start the server:
-start();
+passport.serializeUser(function(user, cb) {
+  cb(null, user.id);
+});
 
-// Check the site, giving quick feedback if it hasn't been set up properly.
-// Start the http service. Accept only requests from localhost, for security.
-// If successful, the handle function is called for each request.
-async function start() {
-  try {
-    await fs.access(root);
-    await fs.access(root + "/index.html");
-    types = defineTypes();
-    paths = new Set();
-    paths.add("/");
-    let service = http.createServer(handle);
-    service.listen(port, "localhost");
-    let address = "http://localhost";
-    if (port != 80) address = address + ":" + port;
-    console.log("Server running at", address);
-  } catch (error) {
-    console.log(error);
-    process.exit(1);
-  }
-}
+passport.deserializeUser(function(id, cb) {
+  find_by_id(id, function (err, user) {
+    if (err) { return cb(err); }
+    cb(null, user);
+  });
+});
 
-// Serve a request by delivering a file.
-async function handle(request, response) {
-  let url = request.url;
-  if (url.endsWith("/")) url = url + "index.html";
-  let ok = await checkPath(url);
-  if (!ok) return fail(response, NotFound, "URL not found (check case)");
-  let type = findType(url);
-  if (type == null) return fail(response, BadType, "File type not supported");
-  let file = root + url;
-  let content = await fs.readFile(file);
-  deliver(response, type, content);
-}
+// Configure view engine to render EJS templates.
+app.set('views', __dirname + '/public');
+app.set('view engine', 'ejs');
 
-// Check if a path is in or can be added to the set of site paths, in order
-// to ensure case-sensitivity.
-async function checkPath(path) {
-  if (!paths.has(path)) {
-    let n = path.lastIndexOf("/", path.length - 2);
-    let parent = path.substring(0, n + 1);
-    let ok = await checkPath(parent);
-    if (ok) await addContents(parent);
-  }
-  return paths.has(path);
-}
+// Use application-level middleware for common functionality, including
+// logging, parsing, and session handling.
+app.use(require('cookie-parser')());
+app.use(require('body-parser').urlencoded({ extended: true }));
+app.use(require('express-session')({ secret: 'keyboard cat', resave: false, saveUninitialized: false }));
 
-// Add the files and subfolders in a folder to the set of site paths.
-async function addContents(folder) {
-  let folderBit = 1 << 14;
-  let names = await fs.readdir(root + folder);
-  for (let name of names) {
-  let path = folder + name;
-  let stat = await fs.stat(root + path);
-  if ((stat.mode & folderBit) != 0) path = path + "/";
-    paths.add(path);
-  }
-}
+// Initialize Passport and restore authentication state, if any, from the
+// session.
+app.use(passport.initialize());
+app.use(passport.session());
 
-// Find the content type to respond with, or undefined.
-function findType(url) {
-  let dot = url.lastIndexOf(".");
-  let extension = url.substring(dot + 1);
-  return types[extension];
-}
+// Define routes.
+app.get('/',
+  function(req, res) {
+    res.render('index', { user: req.user });
+  });
 
-// Deliver the file that has been read in to the browser.
-function deliver(response, type, content) {
-  let typeHeader = { "Content-Type": type };
-  response.writeHead(OK, typeHeader);
-  response.write(content);
-  response.end();
-}
+app.get('/timeline',
+  function(req, res) {
+    res.render('timeline', { user: req.user });
+  });
 
-// Give a minimal failure response to the browser
-function fail(response, code, text) {
-  let textTypeHeader = { "Content-Type": "text/plain" };
-  response.writeHead(code, textTypeHeader);
-  response.write(text, "utf8");
-  response.end();
-}
+app.get('/about',
+  function(req, res){
+    res.render('about', { user: req.user });
+  });
 
-// The most common standard file extensions are supported, and html is
-// delivered as "application/xhtml+xml".  Some common non-standard file
-// extensions are explicitly excluded.  This table is defined using a function
-// rather than just a global variable, because otherwise the table would have
-// to appear before calling start().  NOTE: add entries as needed or, for a more
-// complete list, install the mime module and adapt the list it provides.
-function defineTypes() {
-  let types = {
-    html  : "application/xhtml+xml",
-    css   : "text/css",
-    js    : "application/javascript",
-    mjs   : "application/javascript", // for ES6 modules
-    png   : "image/png",
-    gif   : "image/gif",    // for images copied unchanged
-    jpeg  : "image/jpeg",   // for images copied unchanged
-    jpg   : "image/jpeg",   // for images copied unchanged
-    svg   : "image/svg+xml",
-    json  : "application/json",
-    pdf   : "application/pdf",
-    txt   : "text/plain",
-    ttf   : "application/x-font-ttf",
-    woff  : "application/font-woff",
-    aac   : "audio/aac",
-    mp3   : "audio/mpeg",
-    mp4   : "video/mp4",
-    webm  : "video/webm",
-    ico   : "image/x-icon", // just for favicon.ico
-    xhtml : undefined,      // non-standard, use .html
-    htm   : undefined,      // non-standard, use .html
-    rar   : undefined,      // non-standard, platform dependent, use .zip
-    doc   : undefined,      // non-standard, platform dependent, use .pdf
-    docx  : undefined,      // non-standard, platform dependent, use .pdf
-  }
-  return types;
-}
+app.get('/login',
+  function(req, res){
+    res.render('login');
+  });
 
-// Example function to fetch all animals from a database. Change this to suit
-// our needs.
-async function fetch() {
+app.post('/login',
+  passport.authenticate('local', { failureRedirect: '/login' }),
+  function(req, res) {
+    res.redirect('/');
+  });
+
+app.get('/logout',
+  function(req, res){
+    req.logout();
+    res.redirect('/');
+  });
+
+app.get('/profile',
+  require('connect-ensure-login').ensureLoggedIn(),
+  function(req, res){
+    res.render('profile', { user: req.user });
+  });
+
+// // Serve static files in the 'public' directory.
+app.use(express.static('public'));
+
+// Start the server on port 3000.
+app.listen(process.env.PORT || 3000, function() {
+  console.log('Server listening on port 3000...');
+});
+
+async function find_by_username(username, cb) {
   try {
     var db = await sqlite.open("./db.sqlite");
-    var as = await db.all("select * from animals");
-    console.log(as);
+    // Using prepared statements to prevent SQL injection attacks
+    var user = await db.all("select * from users where username=?", username);
+    if (user.length === 0) {
+      return cb(null, null);
+    } else {
+      return cb(null, user[0]);
+    }
   } catch (error) {
-    console.log(error); }
+    console.log(error);
+  }
 }
 
-// Example function to insert an animal into a database. Change this to suit
-// our needs.
-async function insert() {
-  var db = await sqlite.open("./db.sqlite");
-  // Using prepared statements to prevent SQL injection attacks
-  var ps = db.prepare("insert into animals values (?,?)");
-  await ps.run(64, "cat"); // May need to be single quotes?
-}
-
-// Example function to update a single row in a database. Change this to suit
-// our needs.
-async function update() {
-  var db = await sqlite.open("./db.sqlite");
-  // Using prepared statements to prevent SQL injection attacks
-  var ps = db.prepare("update animals set breed=? where id=?");
-  await ps.run("terrier", 42);
+async function find_by_id(id, cb) {
+  try {
+    var db = await sqlite.open("./db.sqlite");
+    // Using prepared statements to prevent SQL injection attacks
+    var user = await db.all("select * from users where id=?", id);
+    if (user.length === 0) {
+      cb(new Error('User ' + id + ' does not exist'));
+    } else {
+      cb(null, user[id - 1]);
+    }
+  } catch (error) {
+    console.log(error);
+  }
 }
