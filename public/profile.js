c0cc3592d903fe5c387ed18a609b08329244d96c
Ainsley Rutterford <ar16478@my.bristol.ac.uk>
2019-05-22 20:10:13 +0100

    Started generating HTML on the profile page.

--- public/profile.js
+++ public/profile.js
@@ -4,15 +4,22 @@ fetch('/contributions', { method: 'GET' } ).then(handle);
 
 async function handle(response) {
   const json_response = await response.json();
-  console.log(json_response);
-  if (!json_response.errors) {
-  } else {
-    var errors_container = document.querySelector(".errors-container");
-    var html = "<ul>";
-    for (var i = 0; i < json_response.errors.length; i++) {
-      html += "<li>" + json_response.errors[i].msg + "</li>";
-    }
-    html += "</ul>";
-    errors_container.innerHTML = html;
+  var contributions_container = document.querySelector(".contributions");
+  var html = "";
+  for (var i = 0; i < json_response.length; i++) {
+    html += "<div class=\"contribution\">\
+               <div class=\"image-and-description\"> \
+                 <img class=\"contribution-image\" src=\"" + json_response[i].image_source + "\"/> \
+                 <div class=\"date-title-text\"> \
+                   <div class=\"date-title\">\
+                     <div class=\"historical-date\">April 1989</div> \
+                     <div class=\"contribution-title\">" + json_response[i].title + "</div> \
+                   </div>\
+                   <div class=\"description\">"+ json_response[i].description +"</div> \
+                 </div>\
+               </div>\
+               <div class=\"contribution-date\">"+ json_response[i].contribution_date +"</div> \
+             </div>";
   }
+  contributions_container.innerHTML += html;
 }
