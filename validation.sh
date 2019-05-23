d809570c4229041bacc5936be2414de468382d97
Ainsley Rutterford <ar16478@my.bristol.ac.uk>
2019-05-23 01:16:40 +0100

    Updated validator.sh and validated most of profile.ejs

--- validation.sh
+++ validation.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 echo "Running validation check online..."
-java -Dnu.validator.client.content-type=application/xhtml+xml -Dnu.validator.client.port=80 -Dnu.validator.client.host=html5.validator.nu -cp node_modules/vnu-jar/build/dist/vnu.jar nu.validator.client.HttpClient public/index.html
+java -Dnu.validator.client.content-type=application/xhtml+xml -Dnu.validator.client.port=80 -Dnu.validator.client.host=html5.validator.nu -cp node_modules/vnu-jar/build/dist/vnu.jar nu.validator.client.HttpClient public/profile.ejs
 
 # Once we go public, we may need to download files from our server using wget,
 # running the command above, and then deleting the files locally afterwards?
