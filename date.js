a4ec096d17e580536de56773dc011ba05bfa3a86
Ainsley Rutterford <ar16478@my.bristol.ac.uk>
2019-05-27 22:43:30 +0100

    Deleted date.js

--- date.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-
-var moment = require('moment');
-
-var date = moment("00:30:00-20-05-2019", "hh:mm:ss-DD-MM-YYYY");
-
-var time = moment("0000-01-01", "YYYY-MM-DD");
-
-console.log(time.diff(moment(date), 'days'));
